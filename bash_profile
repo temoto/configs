3ddeb515da563669db897759969f06c7120204d3
Sergey Shepelev <temotor@gmail.com>
2012-12-03 18:36:40 +0400

    added bash_profile

---
+++ bash_profile
@@ -0,0 +1,7 @@
+#
+# ~/.bash_profile
+#
+
+uptime 2>/dev/null
+
+[[ -f ~/.bashrc ]] && source ~/.bashrc
--- create-links.sh
+++ create-links.sh
@@ -3,6 +3,7 @@ link="ln -sf"
 src="`pwd`"
 case $0 in /*) src="`dirname $0`";; esac
 
+$link "$src/bash_profile" ~/.bash_profile
 $link "$src/bashrc" ~/.bashrc
 $link "$src/gvimrc" ~/.gvimrc
 $link "$src/inputrc" ~/.inputrc
