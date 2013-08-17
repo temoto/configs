6be0fd498e1f72c1b5cbd14d8093fc466cb602f4
Sergey Shepelev <temotor@gmail.com>
2013-08-17 18:52:46 +0400

    sublime 3 configs

--- create-links.sh
+++ create-links.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-link="ln -nfs"
+link="ln --force --no-dereference --symbolic"
 src=`python -c "import os, sys; print(os.path.dirname(os.path.abspath(os.path.relpath(sys.argv[1]))))" $0`
 
 mkdir -p ~/bin
@@ -12,6 +12,8 @@ $link "$src/gitignore" ~/.gitignore
 $link "$src/gvimrc" ~/.gvimrc
 $link "$src/hgrc" ~/.hgrc
 $link "$src/inputrc" ~/.inputrc
+$link "$src/sublime-text-2" ~/.config/sublime-text-2
+$link "$src/sublime-text-3" ~/.config/sublime-text-3
 $link "$src/terminator" ~/.config/terminator/config
 $link "$src/vim" ~/.vim
 $link "$src/vimrc" ~/.vimrc
